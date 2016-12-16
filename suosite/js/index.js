33c0030300ae21f6259a8c8fbd9048df84dddc72
SCL92 <517303480@qq.com>
2016-12-16 16:35:47 +0800

    -a

---
+++ suosite/js/index.js
@@ -0,0 +1,17 @@
+window.onload =function(){
+	var windW = $(window).innerWidth();
+	var windH = $(window).innerHeight();
+	
+	$("#UI_Designer").css("width",windW+"px");
+	console.log(1)
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
